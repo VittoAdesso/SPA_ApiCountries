6d10e1c798711dd77537abe3bb0f7771e91329ea
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-08 23:01:47 +0200

    some features to index content

--- src/app/countrie/pages/por-capital/por-capital.component.ts
+++ src/app/countrie/pages/por-capital/por-capital.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
-  styleUrls: ['./por-capital.component.css']
+
 })
 export class PorCapitalComponent implements OnInit {
 
