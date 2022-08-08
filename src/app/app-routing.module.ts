ab9d407f0efeb52165ce9acb13def2a5a4a9c191
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-08 23:10:33 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PorCapitalComponent } from './countrie/pages/por-capital/por-capital.component';
+import { PorCountrieComponent } from './countrie/pages/por-countrie/por-countrie.component';
+import { PorRegionComponent } from './countrie/pages/por-region/por-region.component';
+import { SeeCountrieComponent } from './countrie/pages/see-countrie/see-countrie.component';
 
-@NgModule({
+const routes: Routes = [
+  {
+    path: '', 
+    component: PorCountrieComponent, 
+    pathMatch: 'full'
+  }, 
+  {
+    path: 'region',
+    component: PorRegionComponent
+  }, 
+  {
+    path: 'capital',
+    component: PorCapitalComponent
+  }, 
+  {
+    path: 'pais/:id',
+    component: SeeCountrieComponent
+  }, 
+  {
+    path: '**',
+    redirectTo: '' // or i can create 404Component
+  }
+];
+
+@NgModule({ // is a decorator
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
