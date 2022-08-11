50526cfe7ae54bdfc9423754485a2c6510c15db8
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-11 18:28:47 +0200

    add new operator switchmap

--- src/app/countrie/pages/por-region/por-region.component.ts
+++ src/app/countrie/pages/por-region/por-region.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { CountrieService } from '../../services/countrie.service';
 
 @Component({
@@ -12,14 +13,26 @@ export class PorRegionComponent implements OnInit {
   constructor( private activateRoute: ActivatedRoute, private countryService: CountrieService ) { }
 
   ngOnInit(): void {
-    this.activateRoute.params
-      .subscribe( ({id}) => {
-      console.log(id);
 
-      this.countryService.getRegionById(id).subscribe( region => {
-        console.log(region);
-      })
-    })
+    this.activateRoute.params
+      .pipe( 
+        switchMap(({id}) => this.countryService.getRegionById(id))
+      )
+        .subscribe( response => {
+          console.log( response );
+        })
+
+    // second way, little longer
+    // this.activateRoute.params
+    //   .subscribe( ({id}) => {
+    //   console.log(id);
+
+    //   this.countryService.getRegionById(id).subscribe( region => {
+    //     console.log(region);
+    //   })
+    // })
+
+    
   }
 
 }
